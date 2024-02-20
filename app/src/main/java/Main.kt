8cab4f4fd2658ec6c39df8bcaa1698ed84595437
Waqas Ahmed <101014022+WaqasAhmed123@users.noreply.github.com>
2024-02-20 00:54:25 -0800

    oop initiliaze

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -57,4 +57,17 @@ fun main() {
 //    println("You entered: $integer")
 
 
+    //oop
+
+    val c1 = Car()
+
+// Access the properties and add some values to it
+    c1.brand = "Ford"
+    c1.model = "Mustang"
+    c1.year = 1969
+
+    println(c1.brand)   // Outputs Ford
+    println(c1.model)   // Outputs Mustang
+    println(c1.year)
+
 }
