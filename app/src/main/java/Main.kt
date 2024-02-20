4d65c33232a420505237790d656f02980929ec24
Waqas Ahmed <101014022+WaqasAhmed123@users.noreply.github.com>
2024-02-20 00:21:13 -0800

    basic syntax

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,7 +1,60 @@
+    import java.util.Scanner
+    val reader = Scanner(System.`in`)
 fun main() {
 //    println("Hello World")
-    var name="waqas"
-    val value=5
-    println(name)
-    print(value)
+//    var name="waqas"
+//    val value=5
+//    println(name+5)
+//    print(value)
+//    val myNum: Int = 5                // Int
+//    val myDoubleNum: Double = 5.99    // Double
+//    val myLetter: Char = 'D'          // Char
+//    val myBoolean: Boolean = true     // Boolean
+//    val myText: String = "Hello"
+//    val day = 7
+//
+//    val result = when (day) {
+//        1 -> "Monday"
+//        2 -> "Tuesday"
+//        3 -> "Wednesday"
+//        4 -> "Thursday"
+//        5 -> "Friday"
+//        6 -> "Saturday"
+//        7 -> "Sunday"
+//        else -> "Invalid day."
+//    }
+//    println(result)
+
+//    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+//    for (car in cars){
+//        println(car)
+//
+//    }
+
+//    for (chars in 'a'..'x') {
+//        print(chars)
+//    }
+//fun myFunction() {
+//    println("I just got executed!")
+//}
+//    myFunction()
+
+//    fun printFullName(fname: String,lname: String,age: Int) {
+//        println("$fname  $lname  is  $age years old")
+//    }
+//    printFullName(fname = "Waqas", lname = "Ahmed", age = 22)
+
+//    fun myFunction(x: Int, y: Int): Int {
+//        return (x + y)
+//    }
+//    fun myFunction(x: Int, y: Int) = x + y
+//    print(myFunction(5,10))
+//    print("input")
+//var input= readLine()
+//print("enter num")
+//    var integer:Int = reader.nextInt()
+//
+//    println("You entered: $integer")
+
+
 }
